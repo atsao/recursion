36e60c1c61cff1adba9965ca18f9d575796cf4cd
atsao <andrew.tsao8@gmail.com>
2016-05-03 15:51:59 -0700

    parseJSON working for objects without recursion yet

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -15,13 +15,38 @@ var parseJSON = function(json) {
 
   if (json[0] === '{') {
     var result = {};
-    console.log(json);
+    var startIndex = 0; // will be index of where comma is located
     var colonIndex = json.indexOf(':');
-    console.log(colonIndex);
-    if (colonIndex > 0) {
-      var key = json.slice(2, colonIndex - 1);
-      var value = json.slice(colonIndex + 3, json.length - 2);
-      result[key] = value;
+    var endIndex = json.indexOf(',', startIndex); // will be index of next comma and eventually json.length - 2
+
+    if (endIndex === -1) {
+        endIndex = json.length - 1;
+      }
+
+    console.log(json);
+    console.log('Start index init:', startIndex);
+    console.log('Colon index init:', colonIndex);
+    console.log('End index init:', endIndex);
+    
+    while(true) {
+      if (colonIndex > 0) {
+        var key = json.slice(startIndex + 2, colonIndex - 1);
+        var value = json.slice(colonIndex + 3, endIndex - 1);
+        result[key] = value;
+      }
+      startIndex = endIndex;
+      console.log('Start index:', startIndex);
+
+      colonIndex = json.indexOf(':', startIndex);
+      console.log('Colon index:', colonIndex);
+      if (colonIndex === -1) {
+        break;
+      }
+      endIndex = json.indexOf(',', startIndex + 1);
+      console.log('End index:', endIndex);
+      if (endIndex === -1) {
+        endIndex = json.length - 1;
+      }
     }
     
     console.log(result);
